0a77b919e91b733ac4775f388ac510b9af015635
Abindu D <abindhar@tamu.edu>
2020-02-22 02:13:17 -0600

    Serial and Concurrent Requests Tests

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 """
-Pytest testsuite for testing HTTP Status and Response text body to validate server setup and functionality
+Testsuite
+1. For testing HTTP Status and Response text body to validate server setup and functionality
+2. For comparing sequential vs concurrent accesses
 """
-import os, subprocess, sys, time
 import pytest
+import os, subprocess, sys, time
+import asyncio, concurrent.futures
 from http import HTTPStatus
 import requests
 
@@ -28,3 +31,30 @@ def test_get():
     check_get_response("/samplexml", HTTPStatus.OK, '{"example1.xml": "file"}')
     check_get_response("/articles/a/b/a/", HTTPStatus.OK, '{"Abatasa.html": "file"}')
     check_get_response("/sampledir/", HTTPStatus.OK, '{"1.txt": "file", "10.txt": "file", "2.txt": "file", "3.txt": "file", "4.txt": "file", "5.txt": "file", "6.txt": "file", "7.txt": "file", "8.txt": "file", "9.txt": "file"}')
+
+
+def test_sequential_access():
+    url = "http://localhost:8080"  + "/" + "articles/a/b/a/Abatasa.html"
+    start = time.time()
+    num_requests = 20
+    for _ in range(num_requests):
+        r = requests.get(url)
+    elapsed = round(time.time() - start, 3)
+    print(f"Time taken for {num_requests} serial accesses: {elapsed}")
+    assert False
+
+async def concurrent_requests():
+    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+        url = "http://localhost:8080"  + "/" + "articles/a/b/a/Abatasa.html"
+        loop = asyncio.get_event_loop()
+        futures = [loop.run_in_executor(executor, requests.get, url) for _ in range(20)]
+        await asyncio.gather(*futures)
+
+def test_concurrent_access():
+    num_con_requests = 20
+    loop = asyncio.get_event_loop()
+    start = time.time()
+    loop.run_until_complete(concurrent_requests())
+    elapsed = round(time.time() - start, 3)
+    print(f"Time taken for {num_con_requests} concurrent accesses: {elapsed}")
+    assert False
