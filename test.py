3a70630f171b5b07ae34e49c38b8abf8618157f1
Abindu D <abindhar@tamu.edu>
2020-02-22 00:54:55 -0600

    Added test cases

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
+"""
+Pytest testsuite for testing HTTP Status and Response text body to validate server setup and functionality
+"""
 import os, subprocess, sys, time
 import pytest
 from http import HTTPStatus
 import requests
-
 
 def check_get_response(path, expected_status, expected_text = None):
     url = "http://localhost:8080"  + "/" + path
@@ -14,14 +16,15 @@ def check_get_response(path, expected_status, expected_text = None):
         assert expected_text == r.text
 
 def test_get():
-    #check_get_response("/sampledir/", HTTPStatus.OK, '{"1.txt": "file", "10.txt": "file", "2.txt": "file", "3.txt": "file", "4.txt": "file", "5.txt": "file", "6.txt": "file", "7.txt": "file", "8.txt": "file", "9.txt": "file", "10.txt": "file"')
-    #return
+    # Invalid Files / Directory Access
     check_get_response("/invalid_dir/", HTTPStatus.NOT_FOUND)
     check_get_response("/invalidfile.html", HTTPStatus.NOT_FOUND)
     check_get_response("/invalidfile.json", HTTPStatus.NOT_FOUND)
     check_get_response("/invalidfile.xml", HTTPStatus.NOT_FOUND)
-    # Valid Files / Directories
+    # Valid Files / Directories Access and Response text comparison
     check_get_response("/sampledir/1.txt", HTTPStatus.OK, "File1")
-    return
-    check_get_response("dir", HTTPStatus.OK, '{"ff": "file"}')
-    check_get_response('', HTTPStatus.OK, '{"dir": "directory", "ff": "file"}')
+    check_get_response("/sampledir/2.txt", HTTPStatus.OK, "File2")
+    check_get_response("/samplejson/", HTTPStatus.OK, '{"example_1.json": "file", "example_2.json": "file"}')
+    check_get_response("/samplexml", HTTPStatus.OK, '{"example1.xml": "file"}')
+    check_get_response("/articles/a/b/a/", HTTPStatus.OK, '{"Abatasa.html": "file"}')
+    check_get_response("/sampledir/", HTTPStatus.OK, '{"1.txt": "file", "10.txt": "file", "2.txt": "file", "3.txt": "file", "4.txt": "file", "5.txt": "file", "6.txt": "file", "7.txt": "file", "8.txt": "file", "9.txt": "file"}')
